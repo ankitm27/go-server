bada8daf1f0237d9fb3875dbd243de96c66d4779
ankitm27 <anki.malhotra00@gmail.com>
2019-06-10 00:04:19 +0530

    set and get methid added

--- redis/client.go
+++ redis/client.go
@@ -12,7 +12,7 @@ var isConnectionAvailable = false
 
 func RedisClient(host string) *redis.Client {
 	if !isConnectionAvailable {
-		client := redis.NewClient(&redis.Options{
+		client = redis.NewClient(&redis.Options{
 			Addr:     host + ":6379",
 			Password: "",
 			DB:       0,
@@ -21,8 +21,31 @@ func RedisClient(host string) *redis.Client {
 		isConnectionAvailable = true
 	}
 	return client
+
+}
+
+func createRedisQuene(key string, value string) {
+	client1 := RedisClient("localhost")
+	// fmt.Println("client", client)
+	err := client1.Set(key, value, 0).Err()
+	if err != nil {
+		fmt.Println("error in setting the values", err)
+	}
+	fmt.Println("check")
+}
+
+func getRedisData(key string) {
+	client1 := RedisClient("localhost")
+	val, err := client1.Get(key).Result()
+	if err != nil {
+		fmt.Println("error in getting the data", err)
+	}
+	fmt.Println("val", val)
+
 }
 
 func AddDataIntoRedis(data string) {
-	fmt.Println(data)
+	// fmt.Println(client, "check", string(data))
+	createRedisQuene("check", string(data))
+	getRedisData("check")
 }
