e455fad1b2debd722f1a35bf797beadcd40bdefc
shubhamagrawal1906 <shubham.agrawal1906@gmal.com>
2019-06-09 21:40:20 +0530

    [fix] Add server and fix redis client

---
+++ redis/client.go
@@ -0,0 +1,21 @@
+package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func RedisClient(host string) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     host + ":6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	fmt.Println("pong", pong)
+}
