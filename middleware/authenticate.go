32942ab7d15f3d27b06dcd6837f2d5ee9e658a1b
ankitm27 <anki.malhotra00@gmail.com>
2019-06-23 18:23:51 +0530

    condition added when data is not present

--- middleware/authenticate.go
+++ middleware/authenticate.go
@@ -9,17 +9,34 @@ import (
 func AuthenticateData(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Println("excuting middlewareone")
-		data := r.Header["Authorization"]
-		secret := r.Header["Secret"]
-		fmt.Println("secret", secret)
-		// if err != nil {
-		// 	fmt.Println("err", err)
+		// if(!r.Header["Authorization"] || !r.Header["Secret"]){
+		// 	w.Write([]byte("Please provide valid fields"))
 		// }
-		fmt.Println("data1111", data)
-		fmt.Println("header", r.Header)
-		result := database.IsSecretValid(map[string]string{"key": data[0], "secret": secret[0]})
-		fmt.Println("result", result)
-		next.ServeHTTP(w, r)
-		// fmt.Println("excuting middlewaretwo")
+		value, ok := r.Header["Authorization"]
+		value1, ok1 := r.Header["Secret"]
+		fmt.Println("value", value)
+		fmt.Println("value1", value1)
+		if !ok || !ok1 {
+			w.Write([]byte("Please provide all the valid fields"))
+		} else {
+
+			data := r.Header["Authorization"]
+			secret := r.Header["Secret"]
+			fmt.Println("secret", secret)
+			// if err != nil {
+			// 	fmt.Println("err", err)
+			// }
+			fmt.Println("data1111", data)
+			fmt.Println("header", r.Header)
+			result := database.IsSecretValid(map[string]string{"key": data[0], "secret": secret[0]})
+			fmt.Println("result", result)
+			// next.ServeHTTP(w, r)
+			if !result {
+				w.Write([]byte("Please provide correct fields"))
+			} else {
+				next.ServeHTTP(w, r)
+			}
+			// fmt.Println("excuting middlewaretwo")
+		}
 	})
 }
