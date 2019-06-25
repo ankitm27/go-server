6f6a62b4a5532a49d0369d414b0dc04fe8fcc867
ankitm27 <anki.malhotra00@gmail.com>
2019-06-25 16:58:52 +0530

    function arguments changes

--- elasticSearch/service.go
+++ elasticSearch/service.go
@@ -61,48 +61,48 @@ func Ping(ctx context.Context, client *elasticapi.Client, url string) error {
 	return errors.New("elastic client is nil")
 }
 
-func InsertUsers(ctx context.Context, indexName string, docType string) {
+func InsertUsers(ctx context.Context, indexName string, docType string, data interface{}) {
 	// insert data in elasticsearch
 	client, err := NewElasticClient(context.Background(), false, -1)
 	if err != nil {
-		fmt.Println("There is some probelm, Please try after some time")
+		fmt.Println("There is some problem, Please try after some time")
 	}
-	type User struct {
-		UserID    int    `json:"id"`
-		Email     string `json:"email"`
-		FirstName string `json:"firstname"`
-		LastName  string `json:"lastname"`
-		Age       int    `json:"age"`
-		IsActive  bool   `json:"isActive"`
-		Balance   int    `json:"balance"`
-		Phone     string `json:"phone"`
-	}
-	var listUsers []User
-	for index := 1; index < 5; index++ {
+	// type User struct {
+	// 	UserID    int    `json:"id"`
+	// 	Email     string `json:"email"`
+	// 	FirstName string `json:"firstname"`
+	// 	LastName  string `json:"lastname"`
+	// 	Age       int    `json:"age"`
+	// 	IsActive  bool   `json:"isActive"`
+	// 	Balance   int    `json:"balance"`
+	// 	Phone     string `json:"phone"`
+	// }
+	// var listUsers []User
+	// for index := 1; index < 5; index++ {
 
-		user := User{
-			UserID:    index,
-			Email:     fmt.Sprintf("test%d@gmail.com", index),
-			FirstName: fmt.Sprintf("FirstName_%d", index),
-			LastName:  fmt.Sprintf("LastName_%d", index),
-		}
+	// 	user := User{
+	// 		UserID:    index,
+	// 		Email:     fmt.Sprintf("test%d@gmail.com", index),
+	// 		FirstName: fmt.Sprintf("FirstName_%d", index),
+	// 		LastName:  fmt.Sprintf("LastName_%d", index),
+	// 	}
 
-		listUsers = append(listUsers, user)
-	}
+	// 	listUsers = append(listUsers, user)
+	// }
 
-	for _, userObj := range listUsers {
-		_, err := client.Index().Index(indexName).Type(docType).BodyJson(userObj).Do(ctx)
-		if err != nil {
-			fmt.Printf("UserId=%d was nos created. Error : %s \n", userObj.UserID, err.Error())
-			continue
-		}
+	// for _, userObj := range listUsers {
+	_, err = client.Index().Index(indexName).Type(docType).BodyJson(data).Do(ctx)
+	if err != nil {
+		fmt.Printf("UserId=%d was nos created. Error : %s \n", err)
+		// continue
 	}
+	// }
 
 	// Flush data (need for refreshing data in index) after this command possible to do get.
 	client.Flush().Index(indexName).Do(ctx)
 }
 
-func GetAll(ctx context.Context, indexName string) {
+func GetAll(ctx context.Context, indexName string) interface{} {
 	client, err := NewElasticClient(context.Background(), false, -1)
 	if err != nil {
 		fmt.Println("There is some problem, Please try after some time")
@@ -119,7 +119,7 @@ func GetAll(ctx context.Context, indexName string) {
 
 	fmt.Println("search result", searchResult)
 
-	// return convertSearchResultToUsers(searchResult)
+	return convertSearchResultToUsers(searchResult)
 
 }
 
@@ -134,7 +134,7 @@ type User struct {
 	Phone     string `json:"phone"`
 }
 
-func convertSearchResultToUsers(searchResult *elasticapi.SearchResult) []User {
+func convertSearchResultToUsers(searchResult *elasticapi.SearchResult) interface{} {
 	var result []User
 	for _, hit := range searchResult.Hits.Hits {
 		var userObj User
@@ -148,7 +148,7 @@ func convertSearchResultToUsers(searchResult *elasticapi.SearchResult) []User {
 	return result
 }
 
-func GetUserByID(ctx context.Context, userID int, indexName string) User {
+func GetUserByID(ctx context.Context, userID int, indexName string) interface{} {
 	client, err := NewElasticClient(context.Background(), false, -1)
 	if err != nil {
 		fmt.Println("There is some problem, Please try after some time")
@@ -167,14 +167,14 @@ func GetUserByID(ctx context.Context, userID int, indexName string) User {
 	}
 
 	var result = convertSearchResultToUsers(searchResult)
-	if len(result) > 0 {
-		return result[0]
+	if result != nil {
+		return result
 	}
 
 	return User{}
 }
 
-func GetAllActiveUsers(ctx context.Context, indexName string) []User {
+func GetAllActiveUsers(ctx context.Context, indexName string) interface{} {
 	client, err := NewElasticClient(context.Background(), false, -1)
 	if err != nil {
 		fmt.Println("There is some problem, Please try after some time")
